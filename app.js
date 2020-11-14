c198634ade130324280f1d6e1e5d99d5fcc4afc3
Juan Villa <juanestebanvillamemdoza@gmail.com>
2020-11-14 17:30:42 -0400

    Agregando archivo app.js

---
+++ app.js
@@ -0,0 +1,53 @@
+let dia = 1;
+
+switch (dia) {
+    case 0:
+        console.log('Hoy es Domingo.');
+        break;
+
+    case 1:
+        console.log('Hoy es Lunes.');
+        break;
+
+    case 2:
+        console.log('Hoy es Martes.');
+        break;
+
+    case 3:
+        console.log('Hoy es Miércoles.');
+        break;
+
+    case 4:
+        console.log('Hoy es Jueves.');
+        break;
+
+    case 5:
+        console.log('Hoy es Viernes.');
+        break;
+    case 6:
+        console.log('Hoy es Sábado.');
+        break;
+
+    default:
+        console.log('Ese día no existe.');
+}
+
+/*
+let dia = 5;
+
+if (dia === 0) {
+    console.log('Hoy es Domingo.');
+} else if (dia === 1) {
+    console.log('Hoy es Lunes.');
+} else if (dia === 2) {
+    console.log('Hoy es Martes.');
+} else if (dia === 3) {
+    console.log('Hoy es Miércoles.');
+} else if (dia === 4) {
+    console.log('Hoy es Jueves.');
+} else if (dia === 5) {
+    console.log('Hoy es Viernes.');
+} else if (dia === 6) {
+    console.log('Hoy es Sábado.');
+} else { console.log('Ese día no existe.') }
+*/
